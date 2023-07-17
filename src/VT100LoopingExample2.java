676f5cc709a1c0385c942b681a07a366690dcf70
mcar8764 <mcar8764@gmail.com>
2023-07-17 18:12:41 +0900

    0717JavaStudy

--- src/VT100LoopingExample2.java
+++ src/VT100LoopingExample2.java
@@ -3,28 +3,44 @@ public class VT100LoopingExample2 {
 
 	public static void main(String[] args) throws InterruptedException {
 
+		int[][] str = new int[20][40];
+		
 		System.out.println("\033[2J");
 		
 		
-		for(int i =0; i<5000; i++) {
-
+		for(int i =0; i<str.length; i++) {
 			int line = (int)(Math.random()*20 + 1);
-			int column = (int)(Math.random()*40 + 1);
-			int fg = (int)(Math.random()*8 +30);
-			int bg = (int)(Math.random()*8 +40);
-			int ch = (int)(Math.random()*26 + 'A');
-			
-			System.out.printf("\033[%d;%dH",line,column );
-			System.out.printf("\033[%dm",fg);
-			System.out.printf("\033[%dm",bg);
-			System.out.printf("%c", ch);
-			
-			//System.out.printf("\033[1;41H");
-			//System.out.println("\033[0m");
+			str[i]= {line};
+			for(int j=0; j<str.length;i++) {
+				
+				
+				int column = (int)(Math.random()*40 + 1);
+				
+				
+				
+				
+				
+				int fg = (int)(Math.random()*8 +30);
+				int bg = (int)(Math.random()*8 +40);
+				int ch = (int)(Math.random()*26 + 'A');
+				
+				System.out.printf("\033[%d;%dH",line,column );
+				System.out.printf("\033[%dm",fg);
+				System.out.printf("\033[%dm",bg);
+				System.out.printf("%c", ch);
+				
+				
+				
+				//System.out.printf("\033[1;41H");
+				//System.out.println("\033[0m");
 
-			Thread.sleep(10);
+				Thread.sleep(10);
+				
+			}
 
 		}
+		
+		
 		
 		
 
