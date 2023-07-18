110fa6db6a28c69bce38236f3661906522c7262b
mcar8764 <mcar8764@gmail.com>
2023-07-18 18:19:57 +0900

    Modified EpochTime,VT100

--- src/EpochTimeExample2.java
+++ src/EpochTimeExample2.java
@@ -7,7 +7,7 @@ public class EpochTimeExample2 {
 	public static void main(String[] args) {  //>>초단위(/1000)로 바꿔라.
 		long epoch = System.currentTimeMillis()/1000+9*60*60;  
 		                                          //>>9*60*60 우리나라는 9시간을 더해야 하고
-		//System.out.println(epoch);                // 단위를 맞추기 위해 초단위로 환산. 
+		System.out.println("first: "+ epoch);                // 단위를 맞추기 위해 초단위로 환산. 
 		
 		int year = 1970;
 		for (;;) {
@@ -28,6 +28,9 @@ public class EpochTimeExample2 {
 		// 31 28 31 30 31 30 31 31 30 31 30 31
 		System.out.print(year+"년");
 		
+		System.out.println();
+		System.out.println("middle1: "+ epoch); 
+		
 		
 		int month = 0;
 		int[] dayOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
@@ -46,17 +49,29 @@ public class EpochTimeExample2 {
 			break;
 		}
 		
-		
+		System.out.println();
+		System.out.println("middle: "+ epoch);                // 단위를 맞추기 위해 초단위로 환산. 
+
 		int day =0;
 		int hour =0;
 		int minute =0;
 		int second = 0;
 		int week = 0;
 		
-		minute = (int)epoch/60;
-		System.out.println(minute);
-		hour=minute/60;
-		day=(int)epoch/24/60/60;
+		//minute = (int)epoch/60;
+		//System.out.println(minute);
+		//hour=minute/60;
+		
+		
+		// 나머지가 1이 아니면 변수에 +1을 해라 | 30,31이 넘어가는 시점에는 어떻게 계산될까? 다시 1 되어야 하는데..
+		
+		day=(int)epoch/60/60/24;  
+		
+		
+		
+		hour=(int)epoch/60/60%24;
+		minute = (int)epoch/60%60;
+		second = (int)epoch%60%60;
 		
 		System.out.printf("%d년 %d월 %d일 %d시 %d분 %d초",year,month,day,hour,minute,second);
 		System.out.println(epoch);
